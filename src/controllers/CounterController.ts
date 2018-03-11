f4a0b24810062b49c5ee4a1bbb2450e20da38a96
airtoxin <airtoxin@icloud.com>
2018-03-12 00:35:57 +0900

    lint

--- src/controllers/CounterController.ts
+++ src/controllers/CounterController.ts
@@ -4,14 +4,14 @@ const counterRepository = new CounterRepository();
 
 export class CounterController {
   async get(): Promise<number> {
-    return await counterRepository.get();
+    return counterRepository.get();
   }
 
   async patch(delta: number): Promise<number> {
-    return await counterRepository.add(delta);
+    return counterRepository.add(delta);
   }
 
   async delete(): Promise<number> {
-    return await counterRepository.clear();
+    return counterRepository.clear();
   }
 }
